b27865d7549265754ae1e2ca169ebf3c20f6460e
AstroAir <astro_air@126.com>
2022-02-08 09:12:13 +0800

    22w02b

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -4,12 +4,37 @@ echo "Update & Upgrade\n"
 sudo apt-get update
 sudo apt-get upgrade
 
-echo "Set includes && library"
+echo "Set includes & library"
 sudo cp -R $project_path/../includes/configor /usr/include
 sudo cp -R $project_path/../includes/imgui /usr/include
 sudo cp -R $project_path/../includes/spdlog /usr/include
-sudo cp -R $project_path/../includes/sockpp /usr/include
-sudo cp -R $project_path/../libraries/sockpp /usr/lib
 
-sudo apt install libwebsocketpp-dev
+echo "Build sockpp"
+git clone https://github.com/fpagliughi/sockpp
+mkdir build
+cmake -DCMAKE_INSTALL_PREFIX=/usr -S . -B build
+cmake --build build/
+cd build && make -j4
+sudo make install
+cd ../../
+
+git clone https://github.com/ArthurSonzogni/FTXUI
+mkdir build
+cmake -DCMAKE_INSTALL_PREFIX=/usr -S . -B build
+cd build && make -j4
+sudo make install
+sudo cp ftxui-component.a ../../libraries/ftxui
+sudo cp ftxui-dom.a ../../libraries/ftxui
+sudo cp ftxui-screen.a ../../libraries/ftxui
+cd ../../
+
+wget http://www.lua.org/ftp/lua-5.4.4.tar.gz
+tar -xvf lua-5.4.4.tar.gz
+cd lua-5.4.4
+make -j4
+sudo make install
+sudo cp src/liblua.a ../libraries/lua
+cd ..
+
+sudo apt install libwebsocketpp-dev libglfw3-dev libgl-dev
 
